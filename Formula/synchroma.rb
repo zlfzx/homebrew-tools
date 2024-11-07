958a22c0c096ebc296f2631ae6028c52e0521547
Muhammad Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2024-11-07 22:16:37 +0700

    Update Synchroma formula URLs and checksums

--- Formula/synchroma.rb
+++ Formula/synchroma.rb
@@ -7,18 +7,18 @@ class Synchroma < Formula
     if OS.mac?
         if Hardware::CPU.intel?
             url "https://github.com/zlfzx/synchroma/releases/download/v1.0.0/synchroma-darwin-amd64-1.0.0.tar.gz"
-            sha256 "f71eb79db67cb340b8387616816dbc4ad435e905f22596fdf5638e048aae67e4"
+            sha256 "9ff879c12c7a81c9313a45841d8d27843ef0a39084268781295f15d48220db8d"
         elsif Hardware::CPU.arm?
             url "https://github.com/zlfzx/synchroma/releases/download/v1.0.0/synchroma-darwin-arm64-1.0.0.tar.gz"
-            sha256 "ded8cb0898daba9d0dc2cb789509d7a248e8317388a4d8088025bbf6b895f303"
+            sha256 "7c2b5b3434a0bbe0241c571b7f936f8d560986d507525ca40984a6b33a720297"
         end
     elsif OS.linux?
         if Hardware::CPU.intel?
             url "https://github.com/zlfzx/synchroma/releases/download/v1.0.0/synchroma-linux-amd64-1.0.0.tar.gz"
-            sha256 "20738c1fc4988440a6580f9c2e6515db14e4b7857efad8b1f41da4e6d08ed6b2"
+            sha256 "578cbab8f1cf8aa0719d97f3f00cc0cd8d06842defe475989085e8428e42c316"
         elsif Hardware::CPU.arm?
             url "https://github.com/zlfzx/synchroma/releases/download/v1.0.0/synchroma-linux-arm64-1.0.0.tar.gz"
-            sha256 "903cb20b22d905d55ac3c917d04b7238a5586a5b97ddc86d916e55c27a3f377b"
+            sha256 "072775733e7a37d01a78fc871d325a1d28bbdb8db87e9ffa0aeb7fcc45d087a1"
         end
     end
 
